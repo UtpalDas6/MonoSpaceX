2e0c0d8161b1e6ff65b4c7cae42f10ffdb91cab8
Utpal Das <36782722+UtpalDas6@users.noreply.github.com>
2023-06-20 14:21:44 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,8 @@
+// Function to change font to monospace
+function changeFontToMonospace() {
+    document.body.style.fontFamily = "monospace";
+  }
+  
+  // Execute the function when the page has finished loading
+  window.addEventListener("load", changeFontToMonospace);
+  
